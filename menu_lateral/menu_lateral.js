0ec1ee62ff6c0b82df0a5b1426d8f16e8e1de899
Leibril <ariabi.sr@gmail.com>
2024-05-03 17:39:40 -0600

    sección 1 completada en js

--- menu_lateral/menu_lateral.js
+++ menu_lateral/menu_lateral.js
@@ -1,1 +1,26 @@
-..
+let pMenu_lateral = document.querySelector(".section1");
+
+pMenu_lateral.innerHTML =
+` <article class="menu-lateral" >
+
+        <div class="cBot">
+            <a href="CPU.html" class="botMen">CPU</a>
+        </div>
+        <div class="cBot">
+            <a href="GPU.html" class="botMen">GPU</a>
+        </div>
+        <div class="cBot">
+            <a href="MB.html" class="botMen">MB</a>
+        </div>
+        <div class="cBot">
+            <a href="" class="botMen">RAM</a>
+        </div>
+        <div class="cBot">
+            <a href="" class="botMen">HHD,SSD,M2</a>
+        </div>
+        <div class="cBot">
+            <a href="" class="botMen">Fuente</a>
+        </div>
+        </article>`+
+        `<article class="Contenido"></article>`;
+
