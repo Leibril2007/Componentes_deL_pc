6ca190cc0da30eba90d6e9324f0f74f95bbf13de
marianaseven2013 <gmluch@scl.edu.gt>
2024-05-03 18:19:52 -0600

    hheaderYfooter

--- footer/footer.js
+++ footer/footer.js
@@ -1,1 +1,23 @@
-..
+let fouu = document.querySelector(".foou");
+
+fouu.innerHTML = `
+
+<div class="fou">
+    <h1>Contactanos al:</h1>
+    <h2>Tel: 4568-7123</h2>
+<img src="https://i.pinimg.com/564x/8f/e6/7c/8fe67c4f6bc178fe401efbc397d172ae.jpg" alt="250px" width="100px">
+
+<div class="ins">
+<img src="https://i.pinimg.com/564x/63/9b/3d/639b3dafb544d6f061fcddd2d6686ddb.jpg" alt="">
+</div>
+
+<div class="face">
+    <img src="https://i.pinimg.com/564x/4b/bd/3d/4bbd3da0f7e328e9347d3bda52d71b48.jpg" alt="">
+</div>
+    <p>estilosfuenres_scl</p>
+    <h4>ComponentesAG_scl</h4>
+<div class="final">
+    <img src="https://i.pinimg.com/564x/11/cb/a5/11cba5fca7b52324d5a7844174372e93.jpg" alt="">
+</div>
+`
+
