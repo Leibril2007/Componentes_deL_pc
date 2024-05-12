6cfd4daf9c52adefd1ebb3558dea0226f512ba73
Leibril <ariabi.sr@gmail.com>
2024-05-11 21:24:52 -0600

    Asignación de espacios (tableta y telefono)

---
+++ Contenidos/Contenido-gpu/gpu.js
@@ -0,0 +1,33 @@
+let pContenido = document.querySelector(".section1")
+
+pContenido.innerHTML = 
+
+`
+    <h1 class="titContenido">GPU</h1>
+
+    <div class="defini">
+    
+        <p>La unidad de procesamiento de gráficos (GPU) es un circuito electrónico que puede realizar cálculos matemáticos a alta velocidad. Algunas tareas de computación, como la renderización de gráficos, el machine learning (ML) y la edición de videos, requieren la aplicación de operaciones matemáticas similares en un conjunto de datos grande. El diseño de una GPU le permite realizar la misma operación en varios valores en paralelo. Esto vuelve más eficiente el procesamiento de muchas tareas que requieren mucha capacidad de cómputo. <br> <br> Las GPU son la opción ideal para realizar procesamiento paralelo de uso general, pero históricamente no siempre fue así. Como el nombre indica, las GPU se diseñaron inicialmente para una tarea específica: controlar la visualización de imágenes. </p>
+
+    
+
+        <div class="cajonImg">
+            <img src="https://signal.avg.com/hs-fs/hubfs/Blog_Content/Avg/Signal/AVG%20Signal%20Images/cpu_vs_gpu_whats_the_difference_signal/Signal-cpu-vs-gpu-comparison-Hero.jpg?width=1200&name=Signal-cpu-vs-gpu-comparison-Hero.jpg" alt="" class="imgDefin">
+        </div>
+    </div>
+
+    <h1 class="titCarac">Características</h1>
+    <div class="pList">
+
+        <ul>
+            <li>Procesador de comandos. </li>
+            <li>Unidad de Texturizado.</li>
+            <li>DMA.</li>
+        </ul>
+
+        <ul>
+            <li>Raster Output.</li>
+            <li>Unidad Shader.</li>
+        </ul>
+    </div>`;
+
---
+++ Contenidos/Contenido-hhd/hhd.js
@@ -0,0 +1,39 @@
+let pContenido = document.querySelector(".section1")
+
+pContenido.innerHTML = 
+
+`
+    <h1 class="titContenido">HHD, SSD, M2</h1>
+
+    <div class="defini">
+    
+        <p>HHD: Es un disco que se lee de forma mecánica, dentro de ese disco hay sectores, y en dichos sectores se guardan bloques de información (fotos, vídeos, juegos, música...). 
+        <br><br>
+        SSD: Apareció en el año 2003 y comenzó en PC pero ahora se está extrapolando a consolas y otros lugares. Se ha eliminado el disco de lectura y el brazo mecánico que iba de un lado a otro buscando los sectores de información. 
+        <br><br>
+        M.2: Estos son discos duros sólidos con un aumento muy grande en la velocidad. Se conectan a través de los puertos PCIe, lo que les da mucha más velocidad y amplitud de transferencia.
+        </p>
+
+    
+
+        <div class="cajonImg">
+            <img src="https://database.com.tr/wp-content/uploads/2022/02/ssd-secimi-1024x576-1200x675.jpg" alt="" class="imgDefin">
+        </div>
+    </div>
+
+    <h1 class="titCarac">Características</h1>
+    <div class="pList">
+
+        <ul>
+            <li>Velocidad.</li>
+            <li>Consumo de energía.</li>
+            <li>Ruido.</li>
+        </ul>
+
+        <ul>
+            <li>Tamaño.</li>
+            <li>Rendimiento.</li>
+            <li>Compatibilidad.</li>
+        </ul>
+    </div>`;
+
