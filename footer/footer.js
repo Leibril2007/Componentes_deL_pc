5305c02cd91e1308c50b6c0481b237bba3b63131
marianaseven2013 <gmluch@scl.edu.gt>
2024-05-16 07:18:57 -0600

    Re diseño

--- footer/footer.js
+++ footer/footer.js
@@ -2,25 +2,82 @@ let fouu = document.querySelector(".foou");
 
 fouu.innerHTML = `
 
-
-    <div class="fou">
-        <h1>Contactanos al:</h1>
-        <h2>Tel: 4568-7123</h2>
+<div class="Desarrolladores">
+    <div>
+    <img src="https://i.pinimg.com/564x/11/cb/a5/11cba5fca7b52324d5a7844174372e93.jpg" alt="">
     </div>
-
-    <div class="final">
-        <img src="https://i.pinimg.com/564x/11/cb/a5/11cba5fca7b52324d5a7844174372e93.jpg" alt="">
+      <div class="titu">
+    <h2>TecnoComputer</h2>
+      </div>
+      <br>
+      <div class="Desa">
+    <h3>Desarrolladores</h3>
+      </div>
+    <div class="Accesos">
+        <div class="hijos">
+            
+            <h3>Leibril 👤</h3>
+            <br>
+            <a href="https://github.com/Leibril2007">Visita aquí nuestro repositorio ↩</a>
+        </div>
+        <div class="hijos">
+            <br>
+            <h3>LuchG 👤</h3>
+            <br>
+            <a href="https://github.com/marianaseven2013">Visita aquí nuestro repositorio ↩</a>
+        </div>
     </div>
+</div>
+<div class="biblio">
+    <h2>Bibliografías</h2>
+    <div class="botonee">
+        <br>
+        <br>
+        <div class="cpu">
+        <a href="https://blog.coverix.mx/cpu-que-es-y-cual-es-la-funcion-del-procesador">Fuente 1</a>
+        </div>
+        <br>
+        <br>
+        <div class="gpu">
+        <a href="https://www.administracionderedes.com/sistemas-informaticos/disco-duro/#que_es_el_disco_duro_">Fuente 2</a>
+        </div>
+        <br>
+        <br>
+        <div class="ram">
+        <a href="https://www.opirata.com/blog/fuente-de-alimentacion-partes-y-tipos/#:~:text=La%20fuente%20de%20alimentaci%C3%B3n%2C%20abreviada,los%20ordenadores%20necesitan%20para%20funcionar">Fuente 3</a>
+       </div>
+       <br>
+       <br>
+        <div class="MB">
+       <a href="https://aws.amazon.com/es/what-is/gpu/">Fuente 4</a>
+        </div>
+        <br>
+        <br>
+        <div class="Sass">
+       <a href="https://concepto.de/memoria-ram/">Fuente 5</a>
+        </div>
+        <br>
+        <br>
+        <div class="final">
+       <a href="https://www.hp.com/mx-es/shop/tech-takes/que-hace-la-tarjeta-madre">Fuente 6</a>
+        </div>
 
-    
-    <div class="socialMedia">
-        <div class="ins">
-        ComponentesAG_scl
+    </div>
+    <div class="redes">
         <img src="https://cdn.icon-icons.com/icons2/3858/PNG/512/instagram_logo_icon_238280.png" alt="">
-        </div>
+        <img src="https://cdn-icons-png.flaticon.com/256/20/20673.png" alt="">
+        <img src="https://img.freepik.com/vector-premium/nuevo-logotipo-twitter-x-2023-descarga-vector-logotipo-twitter-x_691560-10794.jpg" alt="">
+    </div>
+</div>
+<div class="stacks">
+    <h2>stacks</h2>
+    
+    <div class="logos">
+        <img src="https://i.pinimg.com/236x/0e/7d/4a/0e7d4ad55cdc3db527af6c3d72f41ad0.jpg" alt="">
+        <img src="https://i.pinimg.com/564x/a5/d2/a9/a5d2a913006aee3504a6882ce38f7959.jpg" alt="">
+        <img src="https://i.pinimg.com/564x/69/e2/b3/69e2b3be6001c33141a95557a5f2cbcd.jpg" alt="">
+    </div>
+</div>
 
-        <div class="face">
-        estilosfuenres_scl
-         <img src="https://cdn-icons-png.flaticon.com/256/20/20673.png" alt="">
-        </div>
-    </div>`;
+
+`;
--- header/header.js
+++ header/header.js
@@ -1,29 +1,37 @@
-
 let header = document.querySelector(".header");
 
 header.innerHTML =
  
- ` 
-
-<div class="logo">
-    <img src="https://i.pinimg.com/564x/11/cb/a5/11cba5fca7b52324d5a7844174372e93.jpg" alt="">
-</div>
-
-<div class="sisi">
-    <h3>Componentes del PC</h3>
-</div>
-
-<div class="lista">
-    <a href="">Componentes</a>
-    <a href="">Tipos de PC</a>
-    <a href="">Problemas de PC</a>
-</div>
+    ` <div class="padre-1">
+        <div class="logo">
+            <img src="https://i.pinimg.com/564x/11/cb/a5/11cba5fca7b52324d5a7844174372e93.jpg" alt="">
+        </div>
+        
+        <div class="sisi">
+            <h3>Componentes del PC</h3>
+        </div>
+        </div>
 
 
-<div class="meni">
-    <span class="material-symbols-outlined">
-        menu
-    </span>
-</div>
+        <div class="padre-2">
+        <div class="lista">
+            <a href="" >Componentes</a>
+            <a href="https://github.com/Leibril2007/Componentes_deL_pc">Github</a>
+            <a href="" >Problemas de PC</a>
+        </div>
+        
+        
+        <div class="meni">
+            <span class="material-symbols-outlined">
+                menu
+            </span>
+        </div>
+    </div>
+
+
+
+
+    
+
 
 `;
